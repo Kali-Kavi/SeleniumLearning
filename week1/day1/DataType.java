66d73d0e119d8ebb085b1507a97a9eaf02f9877c
Kali-Kavi <127000364+Kali-Kavi@users.noreply.github.com>
2023-03-05 08:46:16 +0530

    Add files via upload

---
+++ week1/day1/DataType.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = 4;
+		String name = "Rabbit";
+		boolean isanimal = true;
+	
+		System.out.println(name +  " has " + number +" legs? " +  isanimal );
+		
+	}
+
+}
